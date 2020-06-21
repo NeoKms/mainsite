c25538d117f00f87115fc89eb3e6e6357d53fcc1
Jiuglskiy.Vladislav <Jiuglskiy.Vladislav@chtp.net>
2020-06-21 21:35:11 +0300

    init

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="./style.css">
+	<link rel="stylesheet" type="text/css" href="./bootstrap.min.css">
+
+</head>
+<body>
+<div class="container text-center main-body">
+	<div class="text-center">
+				<img src="./keany12.png" class="img-fluid logo-img">
+				<h2> Прости, основной сайт еще в разработке.</h2>
+		<h3>Посмотри другие мои проекты:</h3>
+		<p class="main_link"><a href="./shop/">Магазин стикеров</a>
+		<p class="github_link"><a href="https://github.com/NeoKms/sticker_shop" target="_blank">Github - NeoKms/sticker_shop</a></p>
+		<p class="main_link"><a href="./school/">Онлайн-журнал для школы</a></p>
+		<p class="github_link"><a href="https://github.com/NeoKms/school_journal" target="_blank">Github - NeoKms/school_journal</a></p>
+	</div>
+</div>
+</body>
+</html>
