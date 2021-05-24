861d73d2413217db9c0322896709730f38e048f3
neokms <upachko@gmail.com>
2021-05-24 23:35:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,12 +12,12 @@
 				<img src="./keany12.png" class="img-fluid logo-img">
 				<h2> Прости, основной сайт еще в разработке.</h2>
 		<h3>Посмотри другие мои проекты:</h3>
-		<p class="main_link"><a href="./shop/">Магазин стикеров</a>
+		<p class="main_link"><a href="./shop/"> --> Магазин стикеров</a>
 		<p class="github_link"><a href="https://github.com/NeoKms/sticker_shop" target="_blank">Github - NeoKms/sticker_shop</a></p>
-		<p class="main_link"><a href="./school/">Онлайн-журнал для школы</a></p>
-		<p class="github_link"><a href="https://github.com/NeoKms/school_journal" target="_blank">Github - NeoKms/school_journal</a></p>
 		<p class="main_link"><a href="./school/"> --> Онлайн-журнал для школы</a></p>
 		<p class="github_link"><a href="https://github.com/NeoKms/school_journal" target="_blank">Github - NeoKms/school_journal</a></p>
+		<p class="main_link"><a href="https://d5d1bmusasgbk48d08eo.apigw.yandexcloud.net/"> --> Serverless сокращатель ссылок</a></p>
+		<p class="github_link"><a href="https://github.com/NeoKms/serverless-link-shortener" target="_blank">Github - NeoKms/serverless-link-shortener</a></p>
 	</div>
 </div>
 </body>
